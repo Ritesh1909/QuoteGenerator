b7b7ad0bc3ce2a61938442adfe6487faa65d821a
Ritesh1909 <30571983+Ritesh1909@users.noreply.github.com>
2020-10-12 16:48:13 +0530

    Files added

---
+++ script.js
@@ -0,0 +1,61 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//show Loader
+function showLoadingSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loader
+function removeLoadingSpinner(){
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+// Get quote from Api
+async function getQuote() {
+    showLoadingSpinner();
+    const proxyUrl='https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response = await fetch(proxyUrl+apiUrl);
+        const data = await response.json();
+        if(data.quoteAuthor === ''){
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if(data.quoteText.length > 120){
+            quoteText.classList.add('long-quote');
+        }else{
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        removeLoadingSpinner();
+    } catch(error){
+        console.log(error);
+        getQuote();
+    }
+}
+
+// Tweet the Quote
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click' , getQuote);
+twitterBtn.addEventListener('click' , tweetQuote);
+
+// On Load
+getQuote();
